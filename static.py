d78c172ca55a8b7d848d19166c2911dbf5f7a522
Sheldon McGrandle <developer@rednemesis.com>
2015-02-20 10:38:01 -0500

    can compile all files in a dir now
    Static will compile all files in a dir if passed a dir name instead
    of a file name.

--- static.py
+++ static.py
@@ -2,40 +2,76 @@
 
 r'''
 Usage:
-  html-builder <filename> <destination> <defaultparent> [--var=<KEY::VALUE>...]
+  static FILENAME [DESTINATION] [DEFAULTPARENT] [--var=KEY::VALUE...] [-i IGNORE]
 
 Options:
-  --var=<KEY::VALUE>...  This can be specified multiple times.  Note that
-                         double colons must be used.
+  --var=KEY::VALUE...  This can be specified multiple times.  Note that
+                       double colons must be used.
+  -i PREFIX --ignore-prefix PREFIX
+                       Static will ignore any file starting with
 
 Example:
   html-builder --var='name::John Brown' --var=phone::1234567 \
   content.html ~/projects/site ~/projects/src/html/
 
-filename, destination and defaultparent are required.
+Overview:
+  This takes a child document and inserts it into a parent document.
+  The child document is the <filename> and the parent is the
+  <defaultparent> or from inside of the child document.  The parent
+  must have a [[content]] field in it.  This is where the child
+  document is inserted.
+
+  Values are pass from the child document to the parent by writing
+  template tags like this:
+  <template:some_name>value</template:some_name>
+
+  In the parent the value of some_name is put into this field:
+  [[some_name]]
+
+  The --var=some_name::value field on the command line will be sent to
+  the %%some_name%% field in the parent.
+
+filename is required and the destination and defaultparent can be
+specified on the command line or from inside of the child document.
 
 var is an optional key:value formated string that contains replacement
 values for placeholder strings in the html file.  The placeholder
 looks like this: %%placeholder%%
 
-filename:        Source filename
+filename:        Source filename (the child document)
 destination:     Destination directory for compiled html
 default parent:  The parent html template to use if its not
                  specified in the <template:filename> tag
 '''
 
-import re, sys
+import re, sys, os
 from docopt import docopt
+from pprint import pprint as pp
 
+def error(msg):
+    print msg
+    sys.exit(1)
 
-def main(args):
-    content_filename = args['<filename>']
-    parent_filename = args['<defaultparent>']
-    destination = args['<destination>']
+def compile_all(A):
+    ignore = A.ignore or 'template_'
+    d = A.filename
+    for (dirpath, dirnames, filenames) in os.walk(d):
+        for f in filenames:
+            if f.startswith(ignore): continue
+            child = os.path.join(dirpath, f)
+            A.filename = child
+            A.workingdir = dirpath
+            #pp(vars(A))
+            compile_one(A)
+
+def compile_one(A):
+    content_filename = A.filename
+    parent_filename = A.defaultparent
+    destination = A.destination
 
     vars = {}
-    if args['--var']:
-        for v in args['--var']:
+    if A.var:
+        for v in A.var:
             kv = v.split('::')
             key = kv[0]
             value = kv[1]
@@ -45,8 +81,7 @@ def main(args):
     try:
         content = open(content_filename).read()
     except IOError:
-        print 'No such file or directory:', content_filename
-        sys.exit(1)
+        error('Content file does not exist: "%s"' % content_filename)
 
     # replace the %%...%% fields in the content file
     for m in re.finditer(r'%%(.*?)%%', content):
@@ -62,19 +97,30 @@ def main(args):
         tag_value = m.group(2)
         content = content.replace(m.group(), '') # delete the tag
 
+        # check in the child doc if vars are set there
         if tag_name == 'filename':
             content_filename = tag_value # override command line
         elif tag_name == 'parent':
             parent_filename = tag_value # override command line
+        elif tag_name == 'destination':
+            destination = tag_value
         else:
             tag_values[tag_name] = tag_value
 
+    if not content_filename or not parent_filename or not destination:
+        print('ERROR: %s' % content_filename)
+        print('  Required variables are not set:')
+        #print('    filename:    "%s"' % content_filename)
+        print('    parent:      "%s"' % parent_filename)
+        print('    destination: "%s"' % destination)
+        return
+
     # read the parent html file
+    parent_filename = os.path.join(A.workingdir, parent_filename)
     try:
         frame = open(parent_filename).read()
     except IOError:
-        print 'No such file or directory:', parent_filename
-        sys.exit(1)
+        error('Parent file does not exist: "%s"' % parent_filename)
 
     # replace the %%...%% fields in the parent file
     for m in re.finditer(r'%%(.*?)%%', frame):
@@ -84,7 +130,7 @@ def main(args):
 
     # insert the content into the parent
     content = content.strip()
-    full = frame.replace('[content]', content)
+    full = frame.replace('[[content]]', content)
 
     for v in tag_values:
         dest_tag = '[%s]' % v
@@ -92,11 +138,30 @@ def main(args):
 
 
     # write the full html to the file system
-    dest = open(destination + '/' + content_filename, 'w')
+    destination = os.path.join(A.workingdir, destination, content_filename)
+    try:
+        dest = open(destination, 'w')
+    except IOError:
+        error('Destination does not exist: "%s/%s"' % (destination, content_filename))
     dest.write(full)
     dest.close()
 
-    sys.exit()
+    print 'Done:', content_filename, destination
+
+def main(args):
+    class A:
+        filename = args['FILENAME']
+        destination = args['DESTINATION']
+        defaultparent = args['DEFAULTPARENT']
+        ignore = args['--ignore-prefix']
+        var = args['--var']
+
+    if os.path.isdir(A.filename):
+        compile_all(A)
+    elif os.path.isfile(A.filename):
+        compile_one(A)
+    else:
+        error('%s does not exist' % name)
 
 
 if __name__ == '__main__':
